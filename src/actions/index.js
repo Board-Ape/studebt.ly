217c3442ea757609612879a1ab59ac757fa9cf1f
Sam Singer <samuel.singer1@gmail.com>
2018-02-16 17:38:13 -0700

    Refactor school action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,11 @@
+import { getSchoolData } from '../helper/apiCalls/apiCalls';
+
 export const schoolDataSuccess = (schools) => ({
     type: 'SCHOOL_DATA_SUCCESS',
     schools
 });
 
-export const fetchSchoolData = () => {
-  return async (dispatch) => {
-    const initialFetch = await fetch('https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&_fields=id,school.name,2013.student.size&api_key=ubCKUJm6gZjJgKPmID6SDtnOjKX9exhqhVjYzTBT')
-    const responseObject = await initialFetch.json()
+export const fetchSchoolData = () => async (dispatch) => {
+    const responseObject = await getSchoolData()
     dispatch(schoolDataSuccess(responseObject))
-  }
 }
