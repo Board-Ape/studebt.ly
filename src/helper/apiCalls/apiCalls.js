717cdacea448870c9a73a1c4bf7dc61ed4796119
Sam Singer <samuel.singer1@gmail.com>
2018-02-18 18:48:41 -0700

    Particle landing page present

--- src/helper/apiCalls/apiCalls.js
+++ src/helper/apiCalls/apiCalls.js
@@ -2,7 +2,7 @@ import { SCHOOL_API_KEY } from '../../settings.js'
 
 export const getSchoolData = async () => {
   try {
-  const initialFetch = await fetch(`https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&_fields=id,school.name,2013.student.size&api_key=${SCHOOL_API_KEY}`)
+  const initialFetch = await fetch(`https://api.data.gov/ed/collegescorecard/v1/schools.json?_fields=id,school.faculty_salary,school.school_url,school.name,school.state,school.city,school.zip,2015.cost.tuition.in_state,2015.cost.tuition.out_of_state&_per_page=100&_page=1&api_key=${SCHOOL_API_KEY}`)
   const responseObject = await initialFetch.json();
 
   return responseObject;
